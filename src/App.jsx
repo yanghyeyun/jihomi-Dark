ceb4856032a21d915f55d446e891ac5649354066
jhslive <111948596+jhslive@users.noreply.github.com>
2022-11-04 17:04:15 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -211,7 +211,7 @@ function App() {
               fontSize: "35px",
               color: "rgb(51,51,51)",
               fontFamily: "HarmonyOS Sans",
-              fontWeight: "1500",
+              fontWeight: "800",
 
             }}
           >
