a1e8c8a30f2e5ad1e094a069f2c86012280be87b
jhslive <111948596+jhslive@users.noreply.github.com>
2022-11-07 15:55:45 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -177,11 +177,11 @@ function App() {
             style={{
               position: "absolute",
               right: "125px",
-              top: "196px",
-              fontSize:"42px",
+              top: "176px",
+              fontSize: "35.5px",
               color: "rgb(51,51,51)",
               fontFamily: "Roboto_Regular",
-              fontWeight: "400",
+              fontWeight: "500",
 
             }}
           >
